65c06a7ef835a5bef7b01a6c0704753b9c2f333c
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2023-06-06 09:28:06 -0700

    commit app

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myMenu',
+  webDir: 'dist',
+  bundledWebRuntime:false,
+  
+};
+
+export default config;
